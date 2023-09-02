a7757a76a52efbea01a95011403e6eaca340ac3e
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2023-09-02 16:49:32 +0600

    add-fetching-from-json-fake-server-and-reducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,58 @@
-import Header from "./Header";
+import { useEffect, useReducer } from "react";
 import MainContent from "./MainContent";
+import Header from "./Header";
+
+const initialState = {
+    questions: [],
+    //"loading", "error", "ready", "active", "finished"
+    status: "loading",
+};
+
+function reducer(state, action) {
+    switch (action.type) {
+        case "dataReceived":
+            return {
+                ...state,
+                questions: action.payload,
+                status: "ready",
+            };
+
+        case "dataFailed":
+            return {
+                ...state,
+                status: "error",
+            };
+
+        default:
+            return state;
+    }
+}
 
 function App() {
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    useEffect(() => {
+        async function fetchQuestions() {
+            try {
+                const res = await fetch("http://localhost:3000/questions");
+                const data = await res.json();
+
+                if (!res.ok) {
+                    return dispatch({ type: "dataFailed" });
+                }
+
+                console.log(res.status);
+
+                dispatch({ type: "dataReceived", payload: data });
+            } catch (error) {
+                console.log(error);
+                dispatch({ type: "dataFailed" });
+            }
+        }
+
+        fetchQuestions();
+    }, []);
+
     return (
         <div className="app">
             <Header />
