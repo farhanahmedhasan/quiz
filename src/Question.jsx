df4efa3edc6237c0d5d0ec6924eae99def499174
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2023-09-04 23:40:07 +0600

    extracted-a-options-component

--- src/Question.jsx
+++ src/Question.jsx
@@ -1,15 +1,10 @@
+import Options from "./Options";
+
 export default function Question({ question, onNextQuestion, hasMoreQuestion }) {
-    console.log(question);
     return (
         <div>
             <h4>{question.question}</h4>
-            <div className="options">
-                {question.options.map((option) => (
-                    <button key={option} className="btn btn-option">
-                        {option}
-                    </button>
-                ))}
-            </div>
+            <Options question={question} />
 
             {hasMoreQuestion && (
                 <button className="btn btn-ui" onClick={onNextQuestion}>
